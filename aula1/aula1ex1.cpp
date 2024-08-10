bc88d95833e6b42faaed9929ba32fc127e8797d2
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-08-09 21:20:21 -0300

    iostream

--- aula1/aula1ex1.cpp
+++ aula1/aula1ex1.cpp
@@ -2,10 +2,9 @@
 
 int main() {
 	int num;
-	setlocale(LC_ALL, "portuguese");
-	std::cout << "Digite um numero: "; // Saída
+	std::cout << "Digite um numero: "; // SaÃ­da
 	std::cin >> num; // Entrada 
-	std::cout << "Voce digitou: " << num << std::endl; // Saída
+	std::cout << "Voce digitou: " << num << std::endl; // SaÃ­da
 	std::cout << "Enter para sair...";
 	system("PAUSE");
 	return 0;
