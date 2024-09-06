f2ae7d182c6969f18b5544114909f65c766de028
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-09-06 19:55:39 -0300

    aula3

---
+++ aula3/exemplo01/main.cpp
@@ -0,0 +1,14 @@
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+#include <iostream>
+
+int main() {
+    int array[5] = {1, 2, 3, 4, 5};
+
+    for (int i = 0; i < 5; i++) {
+        std::cout << "Elemento do vetor: " << array[i] << std::endl;
+    }
+
+    return 0;
+}
